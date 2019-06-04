c34a476fd60370760f25c99788e11f28b10ce62d
Hui Zhu <teawater@hyper.sh>
2019-06-04 13:59:16 +0800

    Add protocols

---
+++ protocols/src/lib.rs
@@ -0,0 +1,13 @@
+pub mod agent;
+pub mod agent_grpc;
+mod oci;
+pub mod types;
+pub mod empty;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
