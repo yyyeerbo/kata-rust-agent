yyyeerbo/kata-rust-agent

commit d63884a6f5f1c1fcbf7ad33878c732be09391f0c

log: Add a structured logger
Switch from using the `log` crate to the `slog` crate to provide
"structured logging" capability.

There are a few complications:

- `slog` allows multiple fields with the same name to appear in the log
  output. Since we want unique log fields (like `logrus`), I've
  implemented a custom `slog` `Drain` that collapses the fields into a
  unique set in the correct order.

- The auto-generated gRPC code requires static lifetimes. This
  means we cannot pass (dynamic) logger objects around that part of the
  codebase. To solve this problem, we register a global `slog` logger
  that is accessed via the `sl!()` macro by the gRPC code.

- Some parts of the code don't lend themselves to adding a logger object
  to be passed around. Those areas also make use of a custom `sl!()`
  macro to provide a quick solution to the logger passing issue.

Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

rustjail/src/lib.rs
@@ -28,7 +28,7 @@ extern crate lazy_static;
 extern crate libc;
 extern crate protobuf;
 #[macro_use]
-extern crate log;
+extern crate slog;
 #[macro_use]
 extern crate scan_fmt;
 extern crate oci;

rustjail/src/mount.rs
@@ -27,6 +27,7 @@ use lazy_static;
 use std::string::ToString;
 
 use protobuf::{CachedSize, RepeatedField, UnknownFields};
+use slog::Logger;
 
 // Info reveals information about a particular mounted filesystem. This
 // struct is populated from the content in the /proc/<pid>/mountinfo file.
@@ -97,6 +98,7 @@ lazy_static! {
 }
 
 pub fn init_rootfs(
+    logger: &Logger,
     spec: &Spec,
     cpath: &HashMap<String, String>,
     mounts: &HashMap<String, String>,
@@ -132,7 +134,7 @@ pub fn init_rootfs(
             return Err(ErrorKind::Nix(nix::Error::Sys(Errno::EINVAL)).into());
         }
         if m.field_type == "cgroup" {
-            mount_cgroups(m, rootfs, flags, &data, cpath, mounts)?;
+            mount_cgroups(logger, m, rootfs, flags, &data, cpath, mounts)?;
         } else {
             if m.destination == "/dev" {
                 flags &= !MsFlags::MS_RDONLY;
@@ -155,6 +157,7 @@ pub fn init_rootfs(
 }
 
 fn mount_cgroups(
+    logger: &Logger,
     m: &Mount,
     rootfs: &str,
     flags: MsFlags,
@@ -173,7 +176,7 @@ fn mount_cgroups(
     };
 
     let cflags = MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID | MsFlags::MS_NODEV;
-    info!("tmpfs");
+    info!(logger, "tmpfs");
     mount_from(&ctm, rootfs, cflags, "", "")?;
     let olddir = unistd::getcwd()?;
 
@@ -183,7 +186,7 @@ fn mount_cgroups(
 
     // bind mount cgroups
     for (key, mount) in mounts.iter() {
-        info!("{}", key);
+        info!(logger, "{}", key);
         let source = if cpath.get(key).is_some() {
             cpath.get(key).unwrap()
         } else {
@@ -210,7 +213,7 @@ fn mount_cgroups(
 
         srcs.insert(source.to_string());
 
-        info!("{}", destination.as_str());
+        info!(logger, "{}", destination.as_str());
 
         let bm = Mount {
             source: source.to_string(),
@@ -234,6 +237,7 @@ fn mount_cgroups(
             match unix::fs::symlink(destination.as_str(), &src[1..]) {
                 Err(e) => {
                     info!(
+                        logger,
                         "symlink: {} {} err: {}",
                         key,
                         destination.as_str(),
@@ -438,7 +442,7 @@ fn mount_from(m: &Mount, rootfs: &str, flags: MsFlags, data: &str, _label: &str)
         match fs::create_dir_all(&dir) {
             Ok(_) => {}
             Err(e) => {
-                info!("creat dir {}: {}", dir.to_str().unwrap(), e.to_string());
+                //info!("creat dir {}: {}", dir.to_str().unwrap(), e.to_string());
             }
         }
 
@@ -452,7 +456,7 @@ fn mount_from(m: &Mount, rootfs: &str, flags: MsFlags, data: &str, _label: &str)
         PathBuf::from(&m.source)
     };
 
-    info!("{}, {}", src.to_str().unwrap(), dest.as_str());
+    //info!("{}, {}", src.to_str().unwrap(), dest.as_str());
 
     // ignore this check since some mount's src didn't been a directory
     // such as tmpfs.
@@ -468,7 +472,7 @@ fn mount_from(m: &Mount, rootfs: &str, flags: MsFlags, data: &str, _label: &str)
     match stat::stat(dest.as_str()) {
         Ok(_) => {}
         Err(e) => {
-            info!("{}: {}", dest.as_str(), e.as_errno().unwrap().desc());
+            //info!("{}: {}", dest.as_str(), e.as_errno().unwrap().desc());
         }
     }
 
@@ -481,7 +485,7 @@ fn mount_from(m: &Mount, rootfs: &str, flags: MsFlags, data: &str, _label: &str)
     ) {
         Ok(_) => {}
         Err(e) => {
-            info!("mount error: {}", e.as_errno().unwrap().desc());
+            //info!("mount error: {}", e.as_errno().unwrap().desc());
             return Err(e.into());
         }
     }
@@ -504,7 +508,7 @@ fn mount_from(m: &Mount, rootfs: &str, flags: MsFlags, data: &str, _label: &str)
             None::<&str>,
         ) {
             Err(e) => {
-                info!("remout {}: {}", dest.as_str(), e.as_errno().unwrap().desc());
+                //info!("remout {}: {}", dest.as_str(), e.as_errno().unwrap().desc());
                 return Err(e.into());
             }
             Ok(_) => {}
@@ -609,7 +613,7 @@ fn bind_dev(dev: &LinuxDevice) -> Result<()> {
 
 pub fn finish_rootfs(spec: &Spec) -> Result<()> {
     let olddir = unistd::getcwd()?;
-    info!("{}", olddir.to_str().unwrap());
+    //info!("{}", olddir.to_str().unwrap());
     unistd::chdir("/")?;
     if spec.Linux.is_some() {
         let linux = spec.Linux.as_ref().unwrap();
@@ -654,7 +658,7 @@ fn mask_path(path: &str) -> Result<()> {
         return Err(nix::Error::Sys(Errno::EINVAL).into());
     }
 
-    info!("{}", path);
+    //info!("{}", path);
 
     match mount::mount(
         Some("/dev/null"),
@@ -665,13 +669,13 @@ fn mask_path(path: &str) -> Result<()> {
     ) {
         Err(nix::Error::Sys(e)) => {
             if e != Errno::ENOENT && e != Errno::ENOTDIR {
-                info!("{}: {}", path, e.desc());
+                //info!("{}: {}", path, e.desc());
                 return Err(nix::Error::Sys(e).into());
             }
         }
 
         Err(e) => {
-            info!("{}: {}", path, e.as_errno().unwrap().desc());
+            //info!("{}: {}", path, e.as_errno().unwrap().desc());
             return Err(e.into());
         }
 
@@ -686,7 +690,7 @@ fn readonly_path(path: &str) -> Result<()> {
         return Err(nix::Error::Sys(Errno::EINVAL).into());
     }
 
-    info!("{}", path);
+    //info!("{}", path);
 
     match mount::mount(
         Some(&path[1..]),
@@ -699,13 +703,13 @@ fn readonly_path(path: &str) -> Result<()> {
             if e == Errno::ENOENT {
                 return Ok(());
             } else {
-                info!("{}: {}", path, e.desc());
+                //info!("{}: {}", path, e.desc());
                 return Err(nix::Error::Sys(e).into());
             }
         }
 
         Err(e) => {
-            info!("{}: {}", path, e.as_errno().unwrap().desc());
+            //info!("{}: {}", path, e.as_errno().unwrap().desc());
             return Err(e.into());
         }
 

src/mount.rs
@@ -25,6 +25,7 @@ use std::io::{BufRead, BufReader};
 use crate::device::{get_pci_device_name, get_scsi_device_name, online_device};
 use crate::protocols::agent::Storage;
 use crate::Sandbox;
+use slog::Logger;
 
 const DRIVER9PTYPE: &'static str = "9p";
 const DRIVERVIRTIOFSTYPE: &'static str = "virtio-fs";
@@ -129,7 +130,7 @@ lazy_static! {
 
 // StorageHandler is the type of callback to be defined to handle every
 // type of storage driver.
-type StorageHandler = fn(&Storage, Arc<Mutex<Sandbox>>) -> Result<String>;
+type StorageHandler = fn(&Logger, &Storage, Arc<Mutex<Sandbox>>) -> Result<String>;
 
 // StorageHandlerList lists the supported drivers.
 #[cfg_attr(rustfmt, rustfmt_skip)]
@@ -161,19 +162,28 @@ pub struct BareMount<'a> {
     fs_type: &'a str,
     flags: MsFlags,
     options: &'a str,
+    logger: Logger,
 }
 
 // mount mounts a source in to a destination. This will do some bookkeeping:
 // * evaluate all symlinks
 // * ensure the source exists
 impl<'a> BareMount<'a> {
-    pub fn new(s: &'a str, d: &'a str, fs_type: &'a str, flags: MsFlags, options: &'a str) -> Self {
+    pub fn new(
+        s: &'a str,
+        d: &'a str,
+        fs_type: &'a str,
+        flags: MsFlags,
+        options: &'a str,
+        logger: &Logger,
+    ) -> Self {
         BareMount {
             source: s,
             destination: d,
             fs_type: fs_type,
             flags: flags,
             options: options,
+            logger: logger.new(o!("subsystem" => "baremount")),
         }
     }
 
@@ -214,8 +224,12 @@ impl<'a> BareMount<'a> {
         }
 
         info!(
+            self.logger,
             "mount source={:?}, dest={:?}, fs_type={:?}, options={:?}",
-            self.source, self.destination, self.fs_type, self.options
+            self.source,
+            self.destination,
+            self.fs_type,
+            self.options
         );
         let rc = unsafe { mount(source, dest, fs_type, self.flags.bits(), options) };
 
@@ -232,7 +246,11 @@ impl<'a> BareMount<'a> {
     }
 }
 
-fn ephemeral_storage_handler(storage: &Storage, sandbox: Arc<Mutex<Sandbox>>) -> Result<String> {
+fn ephemeral_storage_handler(
+    logger: &Logger,
+    storage: &Storage,
+    sandbox: Arc<Mutex<Sandbox>>,
+) -> Result<String> {
     let s = sandbox.clone();
     let mut sb = s.lock().unwrap();
     let new_storage = sb.set_sandbox_storage(&storage.mount_point);
@@ -245,10 +263,14 @@ fn ephemeral_storage_handler(storage: &Storage, sandbox: Arc<Mutex<Sandbox>>) ->
         return Err(err.into());
     }
 
-    common_storage_handler(storage)
+    common_storage_handler(logger, storage)
 }
 
-fn local_storage_handler(storage: &Storage, sandbox: Arc<Mutex<Sandbox>>) -> Result<String> {
+fn local_storage_handler(
+    _logger: &Logger,
+    storage: &Storage,
+    sandbox: Arc<Mutex<Sandbox>>,
+) -> Result<String> {
     let s = sandbox.clone();
     let mut sb = s.lock().unwrap();
     let new_storage = sb.set_sandbox_storage(&storage.mount_point);
@@ -276,26 +298,39 @@ fn local_storage_handler(storage: &Storage, sandbox: Arc<Mutex<Sandbox>>) -> Res
     Ok("".to_string())
 }
 
-fn virtio9p_storage_handler(storage: &Storage, _sandbox: Arc<Mutex<Sandbox>>) -> Result<String> {
-    common_storage_handler(storage)
+fn virtio9p_storage_handler(
+    logger: &Logger,
+    storage: &Storage,
+    _sandbox: Arc<Mutex<Sandbox>>,
+) -> Result<String> {
+    common_storage_handler(logger, storage)
 }
 
 // virtiommio_blk_storage_handler handles the storage for mmio blk driver.
 fn virtiommio_blk_storage_handler(
+    logger: &Logger,
     storage: &Storage,
     _sandbox: Arc<Mutex<Sandbox>>,
 ) -> Result<String> {
     //The source path is VmPath
-    common_storage_handler(storage)
+    common_storage_handler(logger, storage)
 }
 
 // virtiofs_storage_handler handles the storage for virtio-fs.
-fn virtiofs_storage_handler(storage: &Storage, _sandbox: Arc<Mutex<Sandbox>>) -> Result<String> {
-    common_storage_handler(storage)
+fn virtiofs_storage_handler(
+    logger: &Logger,
+    storage: &Storage,
+    _sandbox: Arc<Mutex<Sandbox>>,
+) -> Result<String> {
+    common_storage_handler(logger, storage)
 }
 
 // virtio_blk_storage_handler handles the storage for blk driver.
-fn virtio_blk_storage_handler(storage: &Storage, sandbox: Arc<Mutex<Sandbox>>) -> Result<String> {
+fn virtio_blk_storage_handler(
+    logger: &Logger,
+    storage: &Storage,
+    sandbox: Arc<Mutex<Sandbox>>,
+) -> Result<String> {
     let mut storage = storage.clone();
     // If hot-plugged, get the device node path based on the PCI address else
     // use the virt path provided in Storage Source
@@ -311,29 +346,35 @@ fn virtio_blk_storage_handler(storage: &Storage, sandbox: Arc<Mutex<Sandbox>>) -
         storage.source = dev_path;
     }
 
-    common_storage_handler(&storage)
+    common_storage_handler(logger, &storage)
 }
 
 // virtio_scsi_storage_handler handles the storage for scsi driver.
-fn virtio_scsi_storage_handler(storage: &Storage, sandbox: Arc<Mutex<Sandbox>>) -> Result<String> {
+fn virtio_scsi_storage_handler(
+    logger: &Logger,
+    storage: &Storage,
+    sandbox: Arc<Mutex<Sandbox>>,
+) -> Result<String> {
     let mut storage = storage.clone();
 
     // Retrieve the device path from SCSI address.
     let dev_path = get_scsi_device_name(sandbox, &storage.source)?;
     storage.source = dev_path;
 
-    common_storage_handler(&storage)
+    common_storage_handler(logger, &storage)
 }
 
-fn common_storage_handler(storage: &Storage) -> Result<String> {
+fn common_storage_handler(logger: &Logger, storage: &Storage) -> Result<String> {
     // Mount the storage device.
     let mount_point = storage.mount_point.to_string();
 
-    mount_storage(storage).and(Ok(mount_point))
+    mount_storage(logger, storage).and(Ok(mount_point))
 }
 
 // mount_storage performs the mount described by the storage structure.
-fn mount_storage(storage: &Storage) -> Result<()> {
+fn mount_storage(logger: &Logger, storage: &Storage) -> Result<()> {
+    let logger = logger.new(o!("subsystem" => "mount"));
+
     match storage.fstype.as_str() {
         DRIVER9PTYPE | DRIVERVIRTIOFSTYPE => {
             let dest_path = Path::new(storage.mount_point.as_str());
@@ -351,6 +392,7 @@ fn mount_storage(storage: &Storage) -> Result<()> {
     let (flags, options) = parse_mount_flags_and_options(options_vec);
 
     info!(
+        logger,
         "mount storage as: mount-source: {},
                 mount-destination: {},
                 mount-fstype:      {},
@@ -367,6 +409,7 @@ fn mount_storage(storage: &Storage) -> Result<()> {
         storage.fstype.as_str(),
         flags,
         options.as_str(),
+        &logger,
     );
 
     bare_mount.mount()
@@ -400,22 +443,31 @@ fn parse_mount_flags_and_options(options_vec: Vec<&str>) -> (MsFlags, String) {
 // associated operations such as waiting for the device to show up, and mount
 // it to a specific location, according to the type of handler chosen, and for
 // each storage.
-pub fn add_storages(storages: Vec<Storage>, sandbox: Arc<Mutex<Sandbox>>) -> Result<Vec<String>> {
+pub fn add_storages(
+    logger: Logger,
+    storages: Vec<Storage>,
+    sandbox: Arc<Mutex<Sandbox>>,
+) -> Result<Vec<String>> {
     let mut mount_list = Vec::new();
 
     for storage in storages {
-        let handler = match STORAGEHANDLERLIST.get(storage.driver.as_str()) {
+        let handler_name = storage.driver.clone();
+        let logger = logger.new(o!(
+            "subsystem" => "storage",
+            "storage-type" => handler_name.to_owned()));
+
+        let handler = match STORAGEHANDLERLIST.get(&handler_name.as_str()) {
             None => {
                 return Err(ErrorKind::ErrorCode(format!(
                     "Failed to find the storage handler {}",
-                    storage.driver
+                    storage.driver.to_owned()
                 ))
                 .into());
             }
             Some(f) => f,
         };
 
-        let mount_point = match handler(&storage, sandbox.clone()) {
+        let mount_point = match handler(&logger, &storage, sandbox.clone()) {
             // Todo need to rollback the mounted storage if err met.
             Err(e) => return Err(e),
             Ok(m) => m,
@@ -429,12 +481,12 @@ pub fn add_storages(storages: Vec<Storage>, sandbox: Arc<Mutex<Sandbox>>) -> Res
     Ok(mount_list)
 }
 
-fn mount_to_rootfs(m: &INIT_MOUNT) -> Result<()> {
+fn mount_to_rootfs(logger: &Logger, m: &INIT_MOUNT) -> Result<()> {
     let options_vec: Vec<&str> = m.options.clone();
 
     let (flags, options) = parse_mount_flags_and_options(options_vec);
 
-    let bare_mount = BareMount::new(m.src, m.dest, m.fstype, flags, options.as_str());
+    let bare_mount = BareMount::new(m.src, m.dest, m.fstype, flags, options.as_str(), logger);
 
     fs::create_dir_all(Path::new(m.dest)).chain_err(|| "could not creat directory")?;
 
@@ -442,15 +494,20 @@ fn mount_to_rootfs(m: &INIT_MOUNT) -> Result<()> {
         if m.src != "dev" {
             return Err(err.into());
         }
-        error!("Could not mount filesystem from {} to {}", m.src, m.dest);
+        error!(
+            logger,
+            "Could not mount filesystem from {} to {}", m.src, m.dest
+        );
     }
 
     Ok(())
 }
 
-pub fn general_mount() -> Result<()> {
+pub fn general_mount(logger: &Logger) -> Result<()> {
+    let logger = logger.new(o!("subsystem" => "mount"));
+
     for m in INIT_ROOTFS_MOUNTS.iter() {
-        mount_to_rootfs(m)?;
+        mount_to_rootfs(&logger, m)?;
     }
 
     Ok(())
@@ -489,7 +546,7 @@ pub fn get_mount_fs_type(mount_point: &str) -> Result<String> {
     .into())
 }
 
-pub fn get_cgroup_mounts(cg_path: &str) -> Result<Vec<INIT_MOUNT>> {
+pub fn get_cgroup_mounts(logger: &Logger, cg_path: &str) -> Result<Vec<INIT_MOUNT>> {
     let file = File::open(&cg_path)?;
     let reader = BufReader::new(file);
 
@@ -529,7 +586,7 @@ pub fn get_cgroup_mounts(cg_path: &str) -> Result<Vec<INIT_MOUNT>> {
     }
 
     if !has_device_cgroup {
-        warn!("The system didn't support device cgroup, which is dangerous, thus agent initialized without cgroup support!\n");
+        warn!(logger, "The system didn't support device cgroup, which is dangerous, thus agent initialized without cgroup support!\n");
         return Ok(Vec::new());
     }
 
@@ -543,11 +600,13 @@ pub fn get_cgroup_mounts(cg_path: &str) -> Result<Vec<INIT_MOUNT>> {
     Ok(cg_mounts)
 }
 
-pub fn cgroups_mount() -> Result<()> {
-    let cgroups = get_cgroup_mounts(PROCCGROUPS)?;
+pub fn cgroups_mount(logger: &Logger) -> Result<()> {
+    let logger = logger.new(o!("subsystem" => "mount"));
+
+    let cgroups = get_cgroup_mounts(&logger, PROCCGROUPS)?;
 
     for cg in cgroups.iter() {
-        mount_to_rootfs(cg)?;
+        mount_to_rootfs(&logger, cg)?;
     }
 
     // Enable memory hierarchical account.

Cargo.toml
@@ -10,8 +10,6 @@ rustjail = { path = "rustjail" }
 protocols = { path = "protocols" }
 lazy_static = "1.3.0"
 error-chain = "0.12.1"
-log = "0.4.6"
-simple-logging = "2.0.2"
 grpcio = { path = "grpc-rs" }
 protobuf = "2.6.1"
 futures = "0.1.27"
@@ -22,3 +20,13 @@ serde_json = "1.0.39"
 signal-hook = "0.1.9"
 scan_fmt = "0.2.3"
 regex = "1"
+# slog:
+# - Dynamic keys required to allow HashMap keys to be slog::Serialized.
+# - The 'max_*' features allow changing the log level at runtime
+#   (by stopping the compiler from removing log calls).
+slog = { version = "2.5.2", features = ["dynamic-keys", "max_level_trace", "release_max_level_info"] }
+slog-json = "2.3.0"
+slog-async = "2.3.0"
+slog-scope = "4.1.2"
+# for testing
+tempfile = "3.1.0"

Makefile
@@ -21,6 +21,9 @@ COMMIT_NO_SHORT := $(shell git rev-parse --short HEAD 2>/dev/null || true)
 COMMIT := $(if $(shell git status --porcelain --untracked-files=no 2>/dev/null || true),${COMMIT_NO}-dirty,${COMMIT_NO})
 COMMIT_MSG = $(if $(COMMIT),$(COMMIT),unknown)
 
+# Exported to allow cargo to see it
+export VERSION_COMMIT := $(if $(COMMIT),$(VERSION)-$(COMMIT),$(VERSION))
+
 BUILD_TYPE = release
 
 ARCH = $(shell uname -m)
@@ -61,7 +64,7 @@ check:
 	@cargo test --target $(TRIPLE) --$(BUILD_TYPE)
 
 run:
-	@cargo run --target $(TRIPLE) --$(BUILD_TYPE)
+	@cargo run --target $(TRIPLE)
 
 show-summary: show-header
 	@printf "project:\n"

rustjail/Cargo.toml
@@ -18,8 +18,8 @@ prctl = "1.0.0"
 lazy_static = "1.3.0"
 libc = "0.2.58"
 protobuf = "2.6.1"
-log = "0.4.6"
+slog = "2.5.2"
+slog-scope = "4.1.2"
 scan_fmt = "0.2"
 regex = "1.1"
 path-absolutize = { git = "git://github.com/magiclen/path-absolutize.git", tag= "v1.1.3" }
-

rustjail/src/capabilities.rs
@@ -11,6 +11,7 @@ use lazy_static;
 use crate::errors::*;
 use caps::{self, CapSet, Capability, CapsHashSet};
 use protocols::oci::LinuxCapabilities;
+use slog::Logger;
 use std::collections::HashMap;
 
 lazy_static! {
@@ -75,14 +76,14 @@ lazy_static! {
     };
 }
 
-fn to_capshashset(caps: &[String]) -> CapsHashSet {
+fn to_capshashset(logger: &Logger, caps: &[String]) -> CapsHashSet {
     let mut r = CapsHashSet::new();
 
     for cap in caps.iter() {
         let c = CAPSMAP.get(cap);
 
         if c.is_none() {
-            warn!("{} is not a cap", cap);
+            warn!(logger, "{} is not a cap", cap);
             continue;
         }
 
@@ -97,31 +98,37 @@ pub fn reset_effective() -> Result<()> {
     Ok(())
 }
 
-pub fn drop_priviledges(caps: &LinuxCapabilities) -> Result<()> {
+pub fn drop_priviledges(logger: &Logger, caps: &LinuxCapabilities) -> Result<()> {
+    let logger = logger.new(o!("subsystem" => "capabilities"));
+
     let all = caps::all();
 
-    for c in all.difference(&to_capshashset(caps.Bounding.as_ref())) {
+    for c in all.difference(&to_capshashset(&logger, caps.Bounding.as_ref())) {
         caps::drop(None, CapSet::Bounding, *c)?;
     }
 
     caps::set(
         None,
         CapSet::Effective,
-        to_capshashset(caps.Effective.as_ref()),
+        to_capshashset(&logger, caps.Effective.as_ref()),
     )?;
     caps::set(
         None,
         CapSet::Permitted,
-        to_capshashset(caps.Permitted.as_ref()),
+        to_capshashset(&logger, caps.Permitted.as_ref()),
     )?;
     caps::set(
         None,
         CapSet::Inheritable,
-        to_capshashset(caps.Inheritable.as_ref()),
+        to_capshashset(&logger, caps.Inheritable.as_ref()),
     )?;
 
-    if let Err(_) = caps::set(None, CapSet::Ambient, to_capshashset(caps.Ambient.as_ref())) {
-        warn!("failed to set ambient capability");
+    if let Err(_) = caps::set(
+        None,
+        CapSet::Ambient,
+        to_capshashset(&logger, caps.Ambient.as_ref()),
+    ) {
+        warn!(logger, "failed to set ambient capability");
     }
 
     Ok(())

commit 245698dc08f3d7ca30ec3a8c750d832605dfb47a

logging: Re-add log calls
Commit d63884a6f5f1c1fcbf7ad33878c732be09391f0c inadvertently removed a
number of log calls, so add them back, referencing the scoped logger.

Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

rustjail/src/lib.rs
@@ -35,6 +35,13 @@ extern crate oci;
 extern crate path_absolutize;
 extern crate regex;
 
+// Convenience macro to obtain the scope logger
+macro_rules! sl {
+    () => {
+        slog_scope::logger().new(o!("subsystem" => "rustjail"))
+    };
+}
+
 pub mod cgroups;
 pub mod container;
 pub mod errors;

rustjail/src/mount.rs
@@ -442,7 +442,12 @@ fn mount_from(m: &Mount, rootfs: &str, flags: MsFlags, data: &str, _label: &str)
         match fs::create_dir_all(&dir) {
             Ok(_) => {}
             Err(e) => {
-                //info!("creat dir {}: {}", dir.to_str().unwrap(), e.to_string());
+                info!(
+                    sl!(),
+                    "creat dir {}: {}",
+                    dir.to_str().unwrap(),
+                    e.to_string()
+                );
             }
         }
 
@@ -456,7 +461,7 @@ fn mount_from(m: &Mount, rootfs: &str, flags: MsFlags, data: &str, _label: &str)
         PathBuf::from(&m.source)
     };
 
-    //info!("{}, {}", src.to_str().unwrap(), dest.as_str());
+    info!(sl!(), "{}, {}", src.to_str().unwrap(), dest.as_str());
 
     // ignore this check since some mount's src didn't been a directory
     // such as tmpfs.
@@ -472,7 +477,7 @@ fn mount_from(m: &Mount, rootfs: &str, flags: MsFlags, data: &str, _label: &str)
     match stat::stat(dest.as_str()) {
         Ok(_) => {}
         Err(e) => {
-            //info!("{}: {}", dest.as_str(), e.as_errno().unwrap().desc());
+            info!(sl!(), "{}: {}", dest.as_str(), e.as_errno().unwrap().desc());
         }
     }
 
@@ -485,7 +490,7 @@ fn mount_from(m: &Mount, rootfs: &str, flags: MsFlags, data: &str, _label: &str)
     ) {
         Ok(_) => {}
         Err(e) => {
-            //info!("mount error: {}", e.as_errno().unwrap().desc());
+            info!(sl!(), "mount error: {}", e.as_errno().unwrap().desc());
             return Err(e.into());
         }
     }
@@ -508,7 +513,12 @@ fn mount_from(m: &Mount, rootfs: &str, flags: MsFlags, data: &str, _label: &str)
             None::<&str>,
         ) {
             Err(e) => {
-                //info!("remout {}: {}", dest.as_str(), e.as_errno().unwrap().desc());
+                info!(
+                    sl!(),
+                    "remout {}: {}",
+                    dest.as_str(),
+                    e.as_errno().unwrap().desc()
+                );
                 return Err(e.into());
             }
             Ok(_) => {}
@@ -613,7 +623,7 @@ fn bind_dev(dev: &LinuxDevice) -> Result<()> {
 
 pub fn finish_rootfs(spec: &Spec) -> Result<()> {
     let olddir = unistd::getcwd()?;
-    //info!("{}", olddir.to_str().unwrap());
+    info!(sl!(), "{}", olddir.to_str().unwrap());
     unistd::chdir("/")?;
     if spec.Linux.is_some() {
         let linux = spec.Linux.as_ref().unwrap();